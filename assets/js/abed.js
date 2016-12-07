f2f31e0dea874a2c9baeafba3971ccc58aa2a8ae
Luis R. Izquierdo <lrizquierdo@ubu.es>
2016-12-07 10:49:31 +0100

    added page-scroll
    http://alijafarian.com/responsive-page-scrolling-with-jquery-and-bootstr
    ap/

--- assets/js/abed.js
+++ assets/js/abed.js
@@ -51,4 +51,44 @@ $('.dropdown-toggle').click(function (e) {
       target: "#sidebar"
    });
 
+
+   // scroll-page from http://alijafarian.com/responsive-page-scrolling-with-jquery-and-bootstrap/
+   // navigation click actions	
+   $('.scroll-link').on('click', function (event) {
+      event.preventDefault();
+      var sectionID = $(this).attr("data-id");
+      scrollToID('#' + sectionID, 750);
+   });
+   // scroll to top action
+   $('.scroll-top').on('click', function (event) {
+      event.preventDefault();
+      $('html, body').animate({
+         scrollTop: 0
+      }, 'slow');
+   });
+
+   // mobile nav toggle
+   $('#nav-toggle').on('click', function (event) {
+      event.preventDefault();
+      $('#navbar').toggleClass("open");
+   });
 });
+
+// scroll function
+function scrollToID(id, speed) {
+   var offSet = 10;
+   var targetOffset = $(id).offset().top - offSet;
+   var mainNav = $('#navbar');
+   $('html,body').animate({
+      scrollTop: targetOffset
+   }, speed);
+   if (mainNav.hasClass("open")) {
+      mainNav.css("height", "1px").removeClass("in").addClass("collapse");
+      mainNav.removeClass("open");
+   }
+}
+if (typeof console === "undefined") {
+   console = {
+      log: function () {}
+   };
+}
